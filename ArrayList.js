f061768072dcd1339a6d3a3473de393817d0d42e
Arnav Aggarwal <arnavaggrwl@gmail.com>
2016-11-01 22:51:27 -0700

    Add ArrayList
    ArrayList class extends Array. Overwrite push & pop.

---
+++ ArrayList.js
@@ -0,0 +1,37 @@
+//ArrayList.js
+
+class ArrayList extends Array {
+  constructor(...items) {
+    super();
+    this.length = 4;
+    this.count = 0;
+    this.push(...items);
+  }
+  
+  push(...items) {
+    const newCount = this.count + items.length;
+    while(newCount >= this.length) {
+      this.length *= 2;
+    }
+    
+    items.forEach((item, index) => this[this.count + index] = item);
+    this.count += items.length;
+    return items;
+  }
+  
+  pop() {
+    if(this.count === 0) {
+       throw new Error('Popping from an empty ArrayList');
+    }
+    
+    if(this.count <= this.length / 4 && this.length > 4) {
+      this.length /= 2;
+    }
+    
+    const lastIndex = this.count - 1;
+    const value = this[lastIndex];
+    delete this[lastIndex];
+    this.count--;
+    return value;
+  }
+}
