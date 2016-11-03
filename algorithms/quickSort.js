74d2328bee32a20c727505ee55d0bb9ac15d81b2
Arnav Aggarwal <arnavaggrwl@gmail.com>
2016-11-02 23:53:17 -0700

    quickSort Doc: Add TODOs, limitations

--- algorithms/quickSort.js
+++ algorithms/quickSort.js
@@ -1,8 +1,14 @@
 /**
- * mergeSort.js
+ * quickSort.js
+ *
+ * TODO: perhaps implement swap function differently
+ * TODO: tests
+ *
+ * Breaks at just above 2^15 items
  */
 
 Array.prototype.quickSort = function(start = 0, end = this.length) {
+  //Use arrow function to maintain proper 'this' binding
   const swap = (i, j) => {
     const temp = this[i];
     this[i] = this[j];
@@ -31,10 +37,10 @@ Array.prototype.quickSort = function(start = 0, end = this.length) {
   this.quickSort(j + 1, end);
 }
 
-const s = [4, 3, 2];
-s.quickSort();
-console.log(s);
+// const s = [4, 3, 2];
+// s.quickSort();
+// console.log(s);
 
-const s2 = [8, 1, 4, 3, 2, 89, 32, 78, 88, 88, 45, 12, 14, 19, 17];
-s2.quickSort();
-console.log(s2);
+// let s2 = [8, 1, 4, 3, 2, 89, 32, 78, 88, 88, 45, 12, 14, 19, 17, 42]; //2^4 => 16
+// s2.quickSort();
+// console.log(s2);
